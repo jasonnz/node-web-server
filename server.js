a3fcbdac10d73d307de86c903aeccacf5fa1fb0e
Jason Marshall <jasonnz@gmail.com>
2017-02-01 21:55:31 +1300

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,14 +23,14 @@ app.use((req, res, next)=> {
     next();
 });
 
-app.use((req, res, next)=> {
-    res.render('maintenance.hbs', {
-        pageTitle: 'Maintenance Page',
-        maintenanceMsg: 'Sorry the page is currently down for Maintenance!!',
-        descriptionMsg: 'We will be back soon, try again in an hour!!'
-    });
-    next();
-});
+// app.use((req, res, next)=> {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Page',
+//         maintenanceMsg: 'Sorry the page is currently down for Maintenance!!',
+//         descriptionMsg: 'We will be back soon, try again in an hour!!'
+//     });
+//     next();
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -60,6 +61,6 @@ app.get('/bad', (req, res)=> {
     });
 });
 
-app.listen(3000, ()=> {
-    console.log('Server is up on port 3000');
+app.listen(port, ()=> {
+    console.log(`Server is up on port ${port}`);
 });
